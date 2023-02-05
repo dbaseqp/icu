4fb6f4fd0aad62128e54e6dff373f82df86a2573
dbaseqp <gabrielfok128@gmail.com>
2023-02-04 23:21:52 -0800

    start

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-$conf='/etc/apache2/mods-available/userdir.conf'
+conf='/etc/apache2/mods-available/userdir.conf'
 sed -i 's/public_html/./g' $conf
 sed -i 's/.*disabled.*//g' $conf
+
+a2enmod userdir
+systemctl restart apache2
